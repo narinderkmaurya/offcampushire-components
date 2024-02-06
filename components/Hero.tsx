b08fea45873435545a1a46ab5faa31187ec58d98
Narinder Maurya <narinder06@pm.me>
2024-02-07 02:06:25 +0530

    Fixed headings

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,11 +11,11 @@ export default function Hero() {
         <header className=' hero-bg flex items-center justify-center '>
             <div className='w-full flex lg:px-24'>
                 <div className="w-1/2">
-                    <h1 className={instrument.className + ' text-6xl font-bold '}>
+                    <h1 className={instrument.className + ' text-6xl font-bold  hero-heading'}>
                         The future of off-campus <br />
                         placement drives is here
                     </h1>
-                    <p className=' mt-4'>
+                    <p className=' mt-4 leading-9 text-xl tracking-wide'>
                         OffcampusHire is the ultimate destination for freshers <br />
                         to explore, apply & attend off-campus placement drives
                     </p>
